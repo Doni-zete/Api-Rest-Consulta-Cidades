9525825a837cf65ac32360205a841b96f9115dc6
Doni-zete <donizete112@gmail.com>
2021-08-25 23:41:56 -0300

    atu

--- cities-api/src/main/java/com/donizete/citiesapi/countries/Country.java
+++
@@ -1,6 +0,0 @@
-package com.donizete.citiesapi.countries;
-
-public class Country {
-
-
-}
--- cities-api/src/main/java/com/donizete/citiesapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.donizete.citiesapi.repository;
-
-import com.donizete.citiesapi.Country.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
